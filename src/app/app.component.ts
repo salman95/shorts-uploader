a248fd9853b435496735799a7185c36b387e7b0a
salman95 <salman.9515@outlook.com>
2022-05-22 19:22:13 -0500

    Changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PostData } from './post-data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) { }
+
   title = 'shorts-uploader';
+  selectedFile!: File;
+  
+
+  onFileSelected(event: any) {
+    this.selectedFile = <File>event.target.files[0];
+    console.log(this.selectedFile);
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append('file', this.selectedFile, this.selectedFile.name);//converts the file to form data
+    this.http.post('http://localhost:5000/api/files', fd)
+    .subscribe(res => {
+      console.log(res)
+    })
+    console.log(this.selectedFile);
+  }
+  onGetTest() {
+    this.http.get('http://localhost:5000/api/convert-video')
+    .subscribe(res => {
+      console.log(res)
+    })
+  }
 }
